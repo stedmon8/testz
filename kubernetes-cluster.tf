987f47e52c25d90f00fbf0d32513b4dca086fe37
Ashley Davis <ashley@codecapers.com.au>
2020-10-15 09:11:20 +1000

    Scaling up the cluster to 2 nodes.

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
     default_node_pool {
         name            = "default"
-        node_count      = 1
+        node_count      = 2
         vm_size         = "Standard_B2ms"
     }
 
